e85b7d69ba6f96d4003e5e54b5225482b0dc5e6a
GrigoreGabriel <92084992+GrigoreGabriel@users.noreply.github.com>
2023-06-17 00:14:12 +0300

    add stock component final

--- src/app/private/components/add-stock/add-stock.component.ts
+++ src/app/private/components/add-stock/add-stock.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { startWith } from 'rxjs';
 import { ProductService, ProductShort } from 'src/app/public/services/product.service';
+import * as _ from 'lodash'
 
 @Component({
   selector: 'app-add-stock',
@@ -10,25 +12,36 @@ import { ProductService, ProductShort } from 'src/app/public/services/product.se
 export class AddStockComponent implements OnInit {
   productOptions:ProductShort[] = [];
 
-
+  filteredOptions :any;
   success:boolean = false;
 
-  addProductForm = new FormGroup({
-    selectProduct: new FormControl('', Validators.required),
-    
-  });
-constructor(private productService: ProductService)
-{}
+ formGroup!:FormGroup ;
+
+constructor(private productService: ProductService,private fb:FormBuilder){}
 
   ngOnInit(): void {
+
     this.productService.getProductShortDetails().subscribe(products=>{
       this.productOptions=products;
-      console.log(this.productOptions);
-      
-    });
+      this.filteredOptions=products;
+  });
+    this.initForm();
+}
+initForm(){
+  this.formGroup=this.fb.group({
+    'selectedProduct' : ['']
+  })
+  this.formGroup?.get('selectedProduct')?.valueChanges.subscribe(response=>{
+    this.filterData(response)
+  });
+}
+  submitForm() {
+    
   }
-  
 
-  submitForm() {}
-
+filterData(enteredData:any){
+  this.filteredOptions = this.productOptions.filter(item=>{
+    return item.name.toLowerCase().indexOf(enteredData.toLowerCase()) > -1
+  })
+}
 }
