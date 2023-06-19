e20881d58d3429021beea93426909a2658f40725
Grigore Gabriel <gabriel.alex2001.gg@gmail.com>
2023-06-19 22:41:05 +0300

    toast messages on adding favorite product

--- src/app/private/components/favorites/favorites.component.ts
+++ src/app/private/components/favorites/favorites.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HotToastService } from '@ngneat/hot-toast';
 import { UserService } from 'src/app/core/services/user.service';
 import { Product } from 'src/app/public/models/product.model';
 // interface FavoriteProduct{
@@ -23,10 +24,20 @@ export class FavoritesComponent implements OnInit {
       .getFavoriteProducts(this.userId!)
       .subscribe((product: Product[]) => (this.products = product));
   }
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService,
+    private toast:HotToastService
+    ) {}
+
   addToCart(productId:number){}
+
   removeFromFavorites(productId:number){
-    this.userService.removeFavoriteProduct(this.userId,productId).subscribe()
+    this.userService.removeFavoriteProduct(this.userId, productId).subscribe({
+    next: ()=>{
+      this.products=this.products.filter(x=>x.id!==productId)
+      this.toast.success("Product removed from favorites")
+    }});
+      
     }
+    
   }
 
